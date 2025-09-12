5f9795b7de786c6de5c02f1477c416faa5946e04
Vikas Choudhary <vikas.choudhary@chase.io>
2025-09-13 02:46:28 +0530

    Add wildcard ingress support and increase replicas to 3 for load balancing

---
+++ argo/install_helm.sh
@@ -0,0 +1,5 @@
+helm repo add argo https://argoproj.github.io/argo-helm
+helm install argocd argo/argo-cd -n argocd
+
+kubectl -n argocd port-forward svc/argocd-server 8080:80 &
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode
