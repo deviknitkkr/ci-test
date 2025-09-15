a227be133930cfd7abd2f2c46f6c9bf9d966814b
Vikas Choudhary <vikas.choudhary@chase.io>
2025-09-15 19:10:54 +0530

    Changed ingress host

--- argo/install_helm.sh
+++ argo/install_helm.sh
@@ -1,4 +1,5 @@
 helm repo add argo https://argoproj.github.io/argo-helm
+kubectl create namespace argocd
 helm install argocd argo/argo-cd -n argocd
 
 kubectl -n argocd port-forward svc/argocd-server 8080:80 &
