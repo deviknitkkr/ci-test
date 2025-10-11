a884890df204230ebf68cffef1f25b74788c45aa
Vikas Choudhary <vikas.choudhary@chase.io>
2025-10-11 18:37:29 +0530

    Fixed servicemonitor.yaml

--- infra/argo/install_helm.sh
+++ infra/argo/install_helm.sh
@@ -1,6 +1,7 @@
 helm repo add argo https://argoproj.github.io/argo-helm
 kubectl create namespace argocd
-helm install argocd argo/argo-cd -n argocd
+helm upgrade --install argocd argo/argo-cd -n argocd --version 8.6.0
 
-kubectl -n argocd port-forward svc/argocd-server 8080:80 &
+kubectl -n argocd port-forward svc/argocd-server 8081:80 &
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode
+k3d image import ecr-public.aws.com/docker/library/redis:7.2.11-alpine
--- infra/monitoring/install-monitoring.sh
+++ infra/monitoring/install-monitoring.sh
@@ -13,5 +13,10 @@ kubectl create namespace monitoring
 # Install Prometheus stack (includes Grafana, Alertmanager, and Prometheus)
 helm upgrade --install prometheus-stack prometheus-community/kube-prometheus-stack \
   --namespace monitoring \
+  --version 78.0.0 \
   --values prometheus-values.yaml
+
+k3d image import registry.k8s.io/ingress-nginx/kube-webhook-certgen:v1.6.3
+k3d image import registry.k8s.io/kube-state-metrics/kube-state-metrics:v2.17.0
+k3d image import docker.io/library/busybox:1.31.1
 
